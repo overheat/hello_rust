05b42bae19abd63d2bd5e15d0c3e81cd43c14e2d
Aaron Tsui <aaron.tsui@outlook.com>
2026-04-01 06:40:12 +0800

    update rust version to 1.93 and modify no_mangle attribute to unsafe

--- components/rust-hello_rust/src/lib.rs
+++ components/rust-hello_rust/src/lib.rs
@@ -1,4 +1,4 @@
-#[no_mangle]
+#[unsafe(no_mangle)]
 extern "C" fn rust_main() -> i32 {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
